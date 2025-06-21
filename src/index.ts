15fe3bb9f690a5eb6ac9914a0c064f9d1d327324
Papa Black <jakpawlik@gmail.com>
2025-06-21 06:09:11 +0200

    v2.5.2
    Changed files:
    - src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
 import type { IManagerConfig } from './types/manager';
+import { RWSManager } from './managers/RWSManager';
+import { TSConfigHelper } from './helper/TSConfigHelper';
+import type { TSConfigControls, TSConfigData } from './types/tsc';
+import { RWSGenerator } from './models/RWSGenerator';
+import { RWSRunner } from './models/RWSRunner';
+import { RWSWebpackBuilder } from './models/RWSWebpackBuilder';
+import { ConfigHelper } from './helper/ConfigHelper';
 
 export {
-    IManagerConfig
+    RWSManager,
+    TSConfigHelper,
+    RWSGenerator,
+    RWSRunner,
+    RWSWebpackBuilder,
+    ConfigHelper
+}
+
+export type {
+    IManagerConfig,
+    TSConfigControls,
+    TSConfigData
 };
